3e3868e347512edf0a18d45944a1af2794baf90b
Rodrigo Roigues <rs9844660@gmail.com>
2024-09-24 18:17:26 -0300

    feat: configure Gulp for SCSS compilation - Added Gulp tasks to compile SCSS to CSS, integrated source maps for easier debugging, set output style to compressed for production readiness, added watch task to automatically compile SCSS on changes

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourceMaps = require('gulp-sourcemaps');
+
+gulp.task('styles', function compileSass() {
+    return gulp.src('./src/scss/main.scss')
+    .pipe(sourceMaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourceMaps.write('./maps'))
+    .pipe(gulp.dest('./dist'));
+});
+
+exports.default = gulp.parallel('styles');
+exports.watch = function() {
+    gulp.watch('./src/scss/*.scss', { ignoreInitial: false }, gulp.parallel('styles'));
+};
